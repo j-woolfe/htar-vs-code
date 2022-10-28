8e653e859431e96b138de40e291867f37fb9eaa9
Jackson Wain <jwai0004@student.monash.edu>
2022-10-28 16:33:28 +1100

    UPdated README

--- src/diagnostics.ts
+++ src/diagnostics.ts
@@ -6,9 +6,6 @@ import {
     Match
 } from "./rust_interface";
 
-// Sample type alias
-// const TARGET = "Int -> String";
-
 export const POTENTIAL_REPLACEMENT = "potential_replacement";
 
 export function refreshDiagnostics(
@@ -17,8 +14,6 @@ export function refreshDiagnostics(
     context: vscode.ExtensionContext
 ): void {
     console.log("Refreshing diagnostics");
-    // // let doc = vscode.window.visibleTextEditors[0].document;
-    // // let doc = editor.document
     let extensionPath = context.extensionUri.fsPath;
     const diagnostics: vscode.Diagnostic[] = [];
 
@@ -42,13 +37,11 @@ type Target = {
 
 function findTargets(doc: vscode.TextDocument): Target[] {
     const targets: Target[] = [];
-    // TODO: better than this
     for (let lineIndex = 0; lineIndex < doc.lineCount; lineIndex++) {
         const lineOfText = doc.lineAt(lineIndex);
 
         if (lineOfText.text.includes("::")) {
             const start = lineOfText.text.indexOf(":: ");
-            // TODO:: Handle end of line properly
             const end = lineOfText.text.length;
             const text = lineOfText.text.slice(start + 3);
 
@@ -71,9 +64,8 @@ function findTargets(doc: vscode.TextDocument): Target[] {
 function createDiagnostic(match: Match, target: Target, doc: vscode.TextDocument): vscode.Diagnostic {
     const message = `The type ${target.text} could be replaced with ${match.replaced_type}`;
     const severity = vscode.DiagnosticSeverity.Information;
-    // const severity = vscode.DiagnosticSeverity.Error;
     const match_range = new vscode.Range(
-        match.location.start.row ,
+        match.location.start.row,
         match.location.start.col,
         match.location.end.row,
         match.location.end.col
@@ -89,31 +81,11 @@ function createDiagnostic(match: Match, target: Target, doc: vscode.TextDocument
     return diagnostic
 }
 
-// function createDiagnostic(doc: vscode.TextDocument, lineOfText: vscode.TextLine, lineIndex: number): vscode.Diagnostic {
-//     // find where in the line of that the 'emoji' is mentioned
-//     const index = lineOfText.text.indexOf(TARGET);
-
-//     // create range that represents, where in the document the word is
-//     const range = new vscode.Range(lineIndex, index, lineIndex, index + TARGET.length);
-
-//     const diagnostic = new vscode.Diagnostic(range, "This type could be replaced with an alias",
-//         vscode.DiagnosticSeverity.Information);
-//     diagnostic.code = POTENTIAL_REPLACEMENT;
-//     return diagnostic;
-// }
-
 export function subscribeToDocumentChanges(context: vscode.ExtensionContext, aliasDiagnostics: vscode.DiagnosticCollection): void {
 
     if (vscode.window.activeTextEditor) {
         refreshDiagnostics(vscode.window.activeTextEditor.document, aliasDiagnostics, context);
     }
-    // context.subscriptions.push(
-    //     vscode.window.onDidChangeActiveTextEditor(editor => {
-    //         if (editor) {
-    //             refreshDiagnostics(editor.document, aliasDiagnostics, context);
-    //         }
-    //     })
-    // );
 
     context.subscriptions.push(
         // vscode.workspace.onDidChangeTextDocument(e => refreshDiagnostics(e.document, aliasDiagnostics, context))
